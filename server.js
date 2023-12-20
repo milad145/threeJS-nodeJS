bb6a96565fd4238cb2b427925879b3d1123bdc7d
milad <miladaslani1991@gmail.com>
2023-12-21 00:17:29 +0330

    run node.js

--- server.js
+++ server.js
@@ -2,31 +2,37 @@
 // Express server file
 // ===============================================
 // inserting required libs
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-const dotenv = require('dotenv');
-const config = require("config");
+import mongoose from "mongoose";
+// mongoose.Promise = require('bluebird');
+import dotenv from "dotenv";
+import config from "config";
 // ===============================================
-// initiate logs
-const logs = require('./lib/modules/logs');
-logs.initLogs(logs.APP_LOG);
-logs.initSecurityLog();
+// // initiate logs
+// import logs from "./lib/modules/logs.js";
+//
+// logs.initLogs(logs.APP_LOG);
+// logs.initSecurityLog();
 // ===============================================
 // initiate config
 global.SecConfig = dotenv.config().parsed;
 global.EnvConfig = config.get('config');
 // ===============================================
 // connect to mongoose database
-const initiateServer = require('./lib/core/initiateServer');
+import {initiateServer} from "./lib/core/initiateServer.js";
+
 function connectToDatabase() {
     mongoose.set("strictQuery", false);
     mongoose.connect(SecConfig['dbConfig'])
-        .then(() => initiateServer.initiateServer())
+        .then(() => {
+            console.log('connected')
+            initiateServer()
+        } )
         .catch(err => {
             console.error(err)
             return connectToDatabase()
         })
 }
+
 // ===============================================
 connectToDatabase()
 // ===============================================
